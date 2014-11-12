572fce50ca28d724b45ae862770ff73d234aad81
sgoblin <rsuper@sonic.net>
2014-11-12 08:15:23 -0800

    Added greenCards.json and modularized cardFileMod.py

--- cardFileMod.py
+++ cardFileMod.py
@@ -4,41 +4,55 @@ def printAllInDict(dictToBePrinted):
     for each_item in dictToBePrinted:
         print(each_item + ': ' + dictToBePrinted[each_item])
 
-def doSomething():
-    # Initializes redCardsDict with all cards from redCards.json
-    redCardsDict = {}
-    with open("redCards.json", 'r') as redCards:
-        redCardsDict = json.load(redCards)
+def doSomething(cardFile):
+    # Initializes cardsDict with all cards from cardFile
+    cardsDict = {}
+    with open(cardFile, 'r') as cards:
+        cardsDict = json.load(cards)
 
     # Asks the user what they want to do
-    whatYouWouldLikeToDo = input('What would you like to do?\nOptions:\n\t(0) Print all cards\n\t(1) Add a card\n\t(2) Remove a card\n\t(Anything Else) Exit\n')
+    whatYouWouldLikeToDo = input('What would you like to do?\nOptions:\n\t(0) Print all cards\n\t(1) Add a card\n\t(2) Remove a card\n\t(3) Switch card color\n\t(Anything Else) Exit\n')
 
     # What the user tells it is done here
     if whatYouWouldLikeToDo == '0':
-        printAllInDict(redCardsDict)
-        doSomething()
+        printAllInDict(cardsDict)
+        print("---------------------------")
+        doSomething(cardFile)
     elif whatYouWouldLikeToDo == '1':
         newCardTitle = input('Type the name of the card that you would like to add: ')
         newCardDesc = input('Type the description of the card you wish to add: ')
-        with open('redCards.json', 'w') as redCards:
-            redCardsDict[newCardTitle] = newCardDesc
-            json.dump(redCardsDict, redCards)
-        doSomething()
+        with open(cardFile, 'w') as cards:
+            cardsDict[newCardTitle] = newCardDesc
+            json.dump(cardsDict, cards)
+        print("---------------------------")
+        doSomething(cardFile)
     elif whatYouWouldLikeToDo == '2':
         cardToBeRemovedTitle = input('Type the name of the card you wish to remove or type 0986 if you don\'t: ')
         if cardToBeRemovedTitle != '0986':
             areYouSureRemove = input('Are you sure you want to remove ' + cardToBeRemovedTitle + ' from the file? Type the name of the card to confirm: ')
             if areYouSureRemove == cardToBeRemovedTitle:
-                cardToBeRemoved = redCardsDict.pop(cardToBeRemovedTitle)
-                with open('redCards.json', 'w') as redCards:
-                    json.dump(redCardsDict, redCards)
+                cardToBeRemoved = cardsDict.pop(cardToBeRemovedTitle)
+                with open(cardFile, 'w') as cards:
+                    json.dump(cardsDict, cards)
                 print('Just removed ' + cardToBeRemovedTitle + ' from the file.')
             else:
-                doSomething()
+                print("---------------------------")
+                doSomething(cardFile)
         else:
-            doSomething()
+            print("---------------------------")
+            doSomething(cardFile)
+    elif whatYouWouldLikeToDo == '3':
+        chooseColor()
     else:
         print('You said something different. I don\'t like that. Good bye!')
 
+def chooseColor():
+    color = input("Which color of cards would you like to use (red or green, default red)? ")
+    print("---------------------------")
+    if color != "":
+        doSomething(color+"Cards.json")
+    else:
+        doSomething("redCards.json")
+    
 # Executes the program
-doSomething()
+chooseColor()
